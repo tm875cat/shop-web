daf60ee1d82789a76690ba7b9e954d633ab25aec
tm875cat <tm875leter@gmail.com>
2025-05-28 21:47:44 +0800

    部屬

--- shop-web/vite.config.js
+++ shop-web/vite.config.js
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/shop-web/', // 改成gitpage的 repo 名稱
   plugins: [
     vue(),
     vueDevTools(),
